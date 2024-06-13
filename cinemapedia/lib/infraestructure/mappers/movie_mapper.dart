471caec5828c00438c82fbdad5866a08331b3f44
J-Pinos3 <josloco665@gmail.com>
2024-06-13 08:59:39 -0500

    get details of a movie, search movie by its name and search delegate

--- cinemapedia/lib/infraestructure/mappers/movie_mapper.dart
+++ cinemapedia/lib/infraestructure/mappers/movie_mapper.dart
@@ -19,8 +19,8 @@ class MovieMapper{
     popularity:moviedb. popularity,
     posterPath: (moviedb.posterPath != '')?
     'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-    :'no-poster',//'https://easimages.basnop.com/default-image_600.png',
-    releaseDate: moviedb.releaseDate,
+    :'https://easimages.basnop.com/default-image_600.png',
+    releaseDate: moviedb.releaseDate ?? DateTime.parse('1111-11-11'),
     title: moviedb.title,
     video: moviedb.video,
     voteAverage: moviedb.voteAverage,
@@ -41,7 +41,7 @@ class MovieMapper{
     popularity:moviedb. popularity,
     posterPath: (moviedb.posterPath != '')?
     'https://image.tmdb.org/t/p/w500/${moviedb.posterPath}'
-    :'no-poster',//'https://easimages.basnop.com/default-image_600.png',
+    :'https://easimages.basnop.com/default-image_600.png',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
     video: moviedb.video,
--- cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,11 +1,18 @@
-import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
-import 'package:flutter/material.dart';
+import 'dart:collection';
 
-class CustomAppBar extends StatelessWidget {
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/movies/movie_info_procider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class CustomAppBar extends ConsumerWidget {
   const CustomAppBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
     final colors = Theme.of(context).colorScheme;
     final textStyle = Theme.of(context).textTheme.titleMedium;
@@ -24,11 +31,26 @@ class CustomAppBar extends StatelessWidget {
               const Spacer(),
               IconButton(
                 onPressed: (){
-                  showSearch(
-                    context: context,
-                    delegate: SearchMovieDelegate()
-                  );
 
+                  final searchedMovies = ref.read(searchedMoviesProvider);
+                  final searchQuery = ref.read(searchQueryProvider);
+                  
+                  //delete async from onPressed
+                  //and take the future value returned by showSearch
+                  showSearch<Movie?>(
+                    query: searchQuery,
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      initialMovies: searchedMovies,
+                      searchMovies: ref.read(searchedMoviesProvider.notifier).searchMoviesByQuery
+                    )
+                  ).then((movie){
+                    if (movie == null)  return;
+                    context.push('/movie/${movie.id}');
+                  });
+
+
+                    
                 },
                 icon: const Icon(Icons.search),
                 
